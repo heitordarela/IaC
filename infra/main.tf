c2f69a0b5d54c481c7f8f5e2f7adfdd40e903a71
Heitor <ld.heitor@gmail.com>
2023-04-10 12:03:53 -0300

    Projeto Finalizado Curso 2

---
+++ infra/main.tf
@@ -0,0 +1,42 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = var.regiao_aws
+}
+
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "main"
+  }
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0735c191cf914754d"
+  instance_type = var.instancia
+  key_name = var.chave
+  tags = {
+    Name = "Terraform Ansible Python"
+  }
+  #Add Security Group
+}
+
+resource "aws_key_pair" "chaveSSH" {
+  key_name = var.chave
+  public_key = file("${var.chave}.pub")
+}
+
+output "IP_publico" {
+  value = aws_instance.app_server.public_ip
+}
---
+++ infra/variables.tf
@@ -0,0 +1,12 @@
+variable "instancia" {
+  type = string
+}
+variable "regiao_aws" {
+  type = string
+}
+variable "chave" {
+  type = string
+}
+variable "ambiente" {
+  type = string
+}
